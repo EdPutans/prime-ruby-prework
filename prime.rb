74c2454d0455b0c60c57b7931afa449b3f5d3b97
Ed Putans <edputans@gmail.com>
2018-07-22 15:36:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+def prime?(number)
+  one-to-number =[]
+  i=2
+  until i = number-1
+    one-to-number.push(i)
+    i=i+1
+one-to-number.each do |numbah|
+    if number % numbah == 0
+      return false
+    else
+      return true
+    end
+  end
+end
